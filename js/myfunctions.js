fc1252c1d3b009e4b2f084099c0ac5df42ddf560
Logan McAnsh <l.m@loganmcansh.com>
2016-03-26 23:36:25 -0400

    new nav bar
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

---
+++ js/myfunctions.js
@@ -0,0 +1,14 @@
+var $nav = $("nav");
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 30) {
+        $nav.css({"background-color": "rgba(0, 0, 0, 0.5)", "-webkit-backdrop-filter": "blur(10px)"});
+        $nav.addClass("scrolled");
+    }
+    else {
+      $nav.css({"background-color": "rgba(0, 0, 0, 0.0)", "-webkit-backdrop-filter": "blur(0px)"});
+      $nav.removeClass("scrolled");
+    }
+});
